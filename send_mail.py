b30ecb555c9c06cc628069f3c229e94ccca792ea
Pedro Queiroga <psq@cin.ufpe.br>
2019-07-05 16:20:23 -0300

    send_mail usando config.py

--- send_mail.py
+++ send_mail.py
@@ -2,24 +2,20 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from utils import pega_credenciais
+import config
 
 
-OTLK_HOST_ADDR = 'smtp-mail.outlook.com'
-OTLK_PORT_NMBR = 587
 EMAIL_SUBJECT = 'Pergamum Renewal Extravaganza just ran'
-
 
 def send_mail(receiver, string):
     "Sends an email from receiver to receiver"
 
-    prgrnw_email_address,prgrnw_email_password = pega_credenciais('credenciais_email')
-    msg = compose_mail(prgrnw_email_address, receiver, string)
+    msg = compose_mail(config.PRGRNW_EMAIL_ADDR, receiver, string)
     # The actual mail send
-    server = smtplib.SMTP(host=OTLK_HOST_ADDR,
-                          port=OTLK_PORT_NMBR)
+    server = smtplib.SMTP(host=config.PRGRNW_HOST_ADDR,
+                          port=config.PRGRNW_PORT_NMBR)
     server.starttls()
-    server.login(prgrnw_email_address, prgrnw_email_password)
+    server.login(config.PRGRNW_EMAIL_ADDR, config.PRGRNW_EMAIL_PWD)
     server.send_message(msg)
     server.quit()
 
